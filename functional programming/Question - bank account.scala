9e00000ae0f374d487dcd79be39171550ff1311a
Anuki-UCSC <65793111+Anuki-UCSC@users.noreply.github.com>
2020-07-29 05:56:08 +0530

    Update Question - bank account.scala

--- functional programming/Question - bank account.scala
+++ functional programming/Question - bank account.scala
@@ -11,10 +11,19 @@ object Assignment_05_part2 {
         
         var bank:List[Account]=List(acc1,acc2,acc3,acc4,acc5,acc6,acc7)
        
-        for(x <-0 to 7 ){
-              println(bank(x).toString)
-          }
-       
+        val find=(n:String,b:List[Account])=>b.filter(x=>x.nic.equals(n)) // find function- search for given nic and print the account details
+        val overdraft=(b:List[Account])=> b.filter(x=>x.balance<0) // overdraft function-make a list of accounts that the balance<0
+        val balance=(b:List[Account])=> b.reduce((x,y)=>new Account("total amount",0,(x.balance)+(y.balance))) //balance-
+        val interest=(b:List[Account])=>b.map(x=> if(x.balance>0){ new Account(x.nic,x.acnumber,x.balance*1.05)} else {new Account(x.nic,x.acnumber,x.balance*0.9)}) //interest function-
+        def printAc(b:List[Account]) = b.foreach(x=>println(x.toString))
+        
+         printAc(bank)
+         println("\n\nfind : \n\t"+find("987765544v",bank) +"\n\n")
+         println(" overdraft :\n\t"+ overdraft(bank) +"\n\n")
+         println(" total amount in the bank is  :\n\t"+ balance(bank) +"\n\n")
+         val bank2=interest(bank);printAc(bank2)
+         println("\n after the process of interest calculation  :\n\t"+ balance(bank2) +"\n\n")
+         
     }
     
    
@@ -27,7 +36,6 @@ class Account(id:String,n: Int, b: Double) {
     override def toString ="[  "+nic+"  :   "+acnumber +"  :  "+ balance+"  ]"
     def withdraw(a:Double) =this.balance=this.balance-a
     def deposit(a:Double) =this.balance=this.balance+a
-    //def transfer(a:Account,b:Double)= 
+    def transfer(a:Account,b:Double)= {this.withdraw(b); a.deposit(b)}  // transfer function- which transfer the money from this account to a given account.
 }
 }
-
