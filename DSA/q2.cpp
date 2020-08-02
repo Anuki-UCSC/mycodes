dbbbbdc9a6dd36abce6d0406e458e66f22299081
Anuki-UCSC <65793111+Anuki-UCSC@users.noreply.github.com>
2020-08-02 22:49:58 +0530

    Create q2.cpp

---
+++ DSA/q2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    int score[n],mask[n];
+    for(i=0;i<n;i++){
+       cin>>score[i];
+       //cout<<score[i];
+       mask[i]=1;
+    }
+    
+    while()
+    
+    cout<<"Hello World";
+
+    return 0;
+}
